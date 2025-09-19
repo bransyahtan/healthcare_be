fbe5c5bf258287c5f626278e4b48567adb52758f
Sultan <bransyahtan@gmail.com>
2025-09-19 10:54:19 +0700

    create update delete user BE

--- src/modules/users/users.resolver.ts
+++ src/modules/users/users.resolver.ts
@@ -1,6 +1,9 @@
-import { Args, Int, Query, Resolver } from "@nestjs/graphql";
+import { Args, Int, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { CreateUserInput } from "./dto/create-user.input";
+import { UpdateUserInput } from "./dto/update-user.input";
 import {
   UserEntity,
+  UserResponse,
   UsersPaginatedResponse,
   UsersResponse,
 } from "./entities/user.entity";
@@ -42,6 +45,58 @@ export class UsersResolver {
           page: page ?? 1,
           pageSize: pageSize ?? 10,
         },
+      };
+    }
+  }
+
+  @Query(() => UserResponse, { name: "user" })
+  async findById(@Args("id", { type: () => Int }) id: number) {
+    try {
+      const item = await this.usersService.findById(id);
+      return { message: "Berhasil", item };
+    } catch (error) {
+      return {
+        message: error.message || "Gagal",
+        item: null,
+      };
+    }
+  }
+
+  @Mutation(() => UserResponse, { name: "createUser" })
+  async create(@Args("createUserInput") createUserInput: CreateUserInput) {
+    try {
+      const item = await this.usersService.create(createUserInput);
+      return { message: "User berhasil dibuat", item };
+    } catch (error) {
+      return {
+        message: error.message || "Gagal membuat user",
+        item: null,
+      };
+    }
+  }
+
+  @Mutation(() => UserResponse, { name: "updateUser" })
+  async update(@Args("updateUserInput") updateUserInput: UpdateUserInput) {
+    try {
+      const item = await this.usersService.update(updateUserInput);
+      return { message: "User berhasil diupdate", item };
+    } catch (error) {
+      return {
+        message: error.message || "Gagal mengupdate user",
+        item: null,
+      };
+    }
+  }
+
+  @Mutation(() => UserResponse, { name: "deleteUser" })
+  async delete(@Args("id", { type: () => Int }) id: number) {
+    try {
+      const item = await this.usersService.delete(id);
+      return { message: "User berhasil dihapus", item };
+    } catch (error) {
+      return {
+        message: error.message || "Gagal menghapus user",
+        item: null,
       };
     }
   }
